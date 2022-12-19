cb69dcb3c356da7f90311647f73e31d38a663806
onwuahakenneth <onwuahakenneth@gmail.com>
2022-12-19 17:05:17 +0100

    add footer component

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { themes } from "../../themes";
 import { globalStyles } from "../constants";
 
 export const Footer = () => {
   return (
     <FooterContainer>
-      <p>Footer Here..</p>
+      <LinkItem>Community</LinkItem>
+      <LinkItem>Help & Support</LinkItem>
+      <LinkItem>Contact us</LinkItem>
+      <LinkItem>Privacy Policy</LinkItem>
+      <LinkItem>Terms of service</LinkItem>
     </FooterContainer>
   );
 };
@@ -14,7 +19,25 @@ const FooterContainer = styled.div`
   position: fixed;
   bottom: 0;
   left: 0;
-  height: 8vh;
+  height: 96px;
   width: 100vw;
-  background-color: ${globalStyles.colors.blue200}
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: ${globalStyles.colors.blue100};
+
+  @media ${themes.breakpoints.md} {
+    height: 224px;
+    padding: 0px 28px;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-evenly;
+  }
+`;
+
+const LinkItem = styled.p`
+  color: ${globalStyles.colors.blue500};
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
 `;
--- src/layout/layout.component.tsx
+++ src/layout/layout.component.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import { Footer } from "../components/footer";
 import { TopNavigationBar } from "../components/top-nav";
-
+import { useRouter } from "next/router";
 interface Props {
   children: JSX.Element;
 }
 
 export const Layout: React.FC<Props> = ({ children }) => {
+  const router = useRouter();
   return (
     <LayoutContainer>
       <TopNavigationBar />
       {children}
-      {/* <Footer /> */}
+      {router.pathname === "/" && <Footer />}
     </LayoutContainer>
   );
 };
